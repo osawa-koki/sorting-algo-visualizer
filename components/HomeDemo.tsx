e7b8ea5e44888c8caa97d8e14a30aed914be733a
osawa-koki <koki@osawa.dev>
2023-10-19 22:15:44 +0900

    デザインの調整。

--- components/HomeDemo.tsx
+++ components/HomeDemo.tsx
@@ -1,62 +1,48 @@
 import React, { useEffect, useState } from 'react'
 import { HomeCharComponent } from '../src/styles/HomeCharComponent'
+import { Alert } from 'react-bootstrap'
 
 export default function HomeDemo (): JSX.Element {
-  // 文字と背景色をカラフルに設定する。
-  // sorting-algo-visualizerの文字をカラフルにする。
   const [homeDemoChars, setHomeDemoChars] = useState<HomeDemoChars>([
     {
-      // 赤: 青
       char: <HomeCharComponent color={[255, 0, 0]} background_color={[0, 0, 255]}>S</HomeCharComponent>,
       position: 0
     },
     {
-      // 青: 緑
       char: <HomeCharComponent color={[0, 0, 255]} background_color={[0, 255, 0]}>o</HomeCharComponent>,
       position: 1
     },
     {
-      // 緑: 赤
       char: <HomeCharComponent color={[0, 255, 0]} background_color={[255, 0, 0]}>r</HomeCharComponent>,
       position: 2
     },
     {
-      // 紫: 黄
       char: <HomeCharComponent color={[255, 0, 255]} background_color={[255, 255, 0]}>t</HomeCharComponent>,
       position: 3
     },
     {
-      // オレンジ: 紫
       char: <HomeCharComponent color={[255, 165, 0]} background_color={[255, 0, 255]}>i</HomeCharComponent>,
       position: 4
     },
     {
-      // 黒: 水色
       char: <HomeCharComponent color={[0, 0, 0]} background_color={[0, 255, 255]}>n</HomeCharComponent>,
       position: 5
     },
     {
-      // 水色: オレンジ
       char: <HomeCharComponent color={[0, 255, 255]} background_color={[255, 165, 0]}>g</HomeCharComponent>,
       position: 6
     }
   ])
 
   useEffect(() => {
-    if (typeof window !== 'undefined') return
     const interval = setInterval(() => {
-      setHomeDemoChars((prevHomeDemoChars) => {
-        const newHomeDemoChars = [...prevHomeDemoChars]
-        for (let i = 0; i < newHomeDemoChars.length; i++) {
-          if (newHomeDemoChars[i].position !== i) {
-            const tmp = newHomeDemoChars[i]
-            newHomeDemoChars[i] = newHomeDemoChars[i + 1]
-            newHomeDemoChars[i + 1] = tmp
-          }
-        }
-        return newHomeDemoChars
-      })
-    }, 100)
+      const randaomNumber = Math.floor(Math.random() * (homeDemoChars.length - 1))
+      const newHomeDemoChars = [...homeDemoChars]
+      const tmp = newHomeDemoChars[randaomNumber]
+      newHomeDemoChars[randaomNumber] = newHomeDemoChars[randaomNumber + 1]
+      newHomeDemoChars[randaomNumber + 1] = tmp
+      setHomeDemoChars(newHomeDemoChars)
+    }, 1000)
     return () => { clearInterval(interval) }
   }, [])
 
@@ -72,6 +58,7 @@ export default function HomeDemo (): JSX.Element {
           )
         })}
       </div>
+      <Alert variant='info' className='w-100 text-center'>💦💦💦 整列アルゴリズムを可視化する！ 💦💦💦</Alert>
     </>
   )
 }
