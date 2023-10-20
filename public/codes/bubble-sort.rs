6504fea437564d569df738ed825c8f2df437356b
osawa-koki <koki@osawa.dev>
2023-10-20 22:04:55 +0900

    コード書き換えた。

--- public/codes/bubble-sort.rs
+++ public/codes/bubble-sort.rs
@@ -1,21 +1,19 @@
-fn bubble_sort(array: &mut [i32]) {
-    let array_length = array.len();
-    // Loop through the array.
-    for i in 0..array_length {
-        // Loop through the array again.
-        for j in 0..array_length - i - 1 {
-            // If the current value is greater than the next value, swap them.
-            if array[j] > array[j + 1] {
-                let temp_val = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp_val;
+fn bubble_sort(mut array: [i32; 8]) -> [i32; 8] {
+    let mut swapped = true;
+    while swapped {
+        swapped = false;
+        for i in 0..array.len() - 1 {
+            if array[i] > array[i + 1] {
+                array.swap(i, i + 1);
+                swapped = true;
             }
         }
     }
+    array
 }
 
 fn main() {
-    let mut array = [2, 5, 8, 1, 4, 3, 7, 6];
-    bubble_sort(&mut array);
-    println!("{:?}", array);
+    let array = [2, 5, 8, 1, 4, 3, 7, 6];
+    let sorted_array = bubble_sort(array);
+    println!("{:?}", sorted_array);
 }
