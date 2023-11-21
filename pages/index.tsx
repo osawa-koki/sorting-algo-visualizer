a7e00d67eef3e907e8c5b03bf9cb9805da9b5979
osawa-koki <koki@osawa.dev>
2023-11-21 21:47:59 +0900

    img -> Image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import setting from '../setting'
 import HomeDemo from '../components/HomeDemo'
+import Image from 'next/image'
 
 export default function Home (): JSX.Element {
   return (
     <>
       <div id='Index' className='d-flex flex-column align-items-center'>
         <h1>{setting.title}</h1>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img id='Logo' className='mt-3' style={{ width: '100%', maxWidth: '100px' }} src={`${setting.basePath}/tako.png`} alt='Logo' />
+        <Image id='Logo' className='mt-3 mw-100 border rounded-circle' width={100} height={100} src={`${setting.basePath}/tako.png`} alt='Logo' />
         <HomeDemo />
       </div>
     </>
